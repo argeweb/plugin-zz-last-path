5287d48378e566d68001aa639a74e5a4fd7e3d0d
cwen0708 <cwen0708@gmail.com>
2017-06-13 01:21:00 +0800

    actions...

--- __init__.py
+++ __init__.py
@@ -10,5 +10,14 @@
 plugins_helper = {
     'title': u'Html 路徑映射',
     'desc': u"根據 routing 的規則，此模組的路徑將會在最後被載入",
+    'controllers': {
+        'zz_last_path': {
+            'group': u'路徑映射',
+            'actions': [
+                {'action': 'config', 'name': u'路徑映射設定'},
+                {'action': 'plugins_check', 'name': u'啟用停用模組'},
+            ]
+        },
+    },
     'install_uri': 'zz_last_path:zz_last_path:after_install'
 }
